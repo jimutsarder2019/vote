525b1aee8bd437303bbb6e89000d0896bb5e95ce
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-15 20:36:07 +0600

    fix

--- views/visitor/view.php
+++ views/visitor/view.php
@@ -11,31 +11,45 @@ $this->params['breadcrumbs'][] = ['label' => 'Visitors', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="visitor-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="page-body">
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="page-header">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="page-header-left">
+						<h3><?= Html::encode($this->title) ?></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'ip',
-            'lat',
-            'lng',
-            'date',
-            'time',
-        ],
-    ]) ?>
-
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xl-6 xl-100">
+				<div class="card">
+					<div class="card-body">
+						<div class="router-create">
+						           <?= DetailView::widget([
+										'model' => $model,
+										'attributes' => [
+											'id',
+											'ip',
+											'lat',
+											'lng',
+											'date',
+											'time',
+										],
+									]) ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 </div>
