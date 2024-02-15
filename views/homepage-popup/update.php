76d8c9d302c753510a79133a6c246044fb9bbdff
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-16 01:49:06 +0600

    add

--- views/homepage-popup/update.php
+++ views/homepage-popup/update.php
@@ -10,12 +10,37 @@ $this->params['breadcrumbs'][] = ['label' => 'Homepage Popups', 'url' => ['index
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="homepage-popup-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+<div class="page-body">
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="page-header">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="page-header-left">
+						    <h3><?= Html::encode($this->title) ?></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
+
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-body">
+						   <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 </div>
