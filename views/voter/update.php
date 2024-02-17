b0d3229d7580bd43c42241c8cca32fab7b110a69
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-18 02:03:25 +0600

    fix

--- views/voter/update.php
+++ views/voter/update.php
@@ -10,12 +10,37 @@ $this->params['breadcrumbs'][] = ['label' => 'Voters', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="voter-update">
+<div class="page-body">
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="page-header">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="page-header-left">
+						    <h3><?= Html::encode($this->title) ?></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xl-6 xl-100">
+				<div class="card">
+					<div class="card-body">
+						<div class="router-create">
+							<?= $this->render('_form', [
+								'model' => $model,
+							]) ?>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 </div>
