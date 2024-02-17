6bc29e3032e296acdb2f77dd97a5c1007e9a8a1f
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-18 01:56:33 +0600

    fix

--- views/voter/index.php
+++ views/voter/index.php
@@ -13,43 +13,64 @@ use yii\grid\GridView;
 $this->title = 'Voters';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="voter-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
+<div class="page-body">
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="page-header">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="page-header-left">
+						<h3><?= Html::encode($this->title) ?></h3>
+						<p>
         <?= Html::a('Create Voter', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+	<!-- Container-fluid starts-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-body">
+						<div class="user-status table-responsive latest-order-table">
+							<?= GridView::widget([
+								'dataProvider' => $dataProvider,
+								'filterModel' => $searchModel,
+								'columns' => [
+									['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'company',
-            'name',
-            'ispab_member',
-            'voter_no',
-            //'mobile1',
-            //'mobile2',
-            //'email:email',
-            //'thana',
-            //'district',
-            //'address',
-            //'license',
-            //'date',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Voter $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-
+									'id',
+									'company',
+									'name',
+									'ispab_member',
+									'voter_no',
+									//'mobile1',
+									//'mobile2',
+									//'email:email',
+									//'thana',
+									//'district',
+									//'address',
+									//'license',
+									//'date',
+									[
+										'class' => ActionColumn::className(),
+										'urlCreator' => function ($action, Voter $model, $key, $index, $column) {
+											return Url::toRoute([$action, 'id' => $model->id]);
+										 }
+									],
+								],
+							]); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Container-fluid Ends-->
 </div>
