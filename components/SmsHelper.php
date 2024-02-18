16334cee768c1791b7a33d36e380cfe0cdc9fcd5
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-18 20:02:49 +0600

    fix

--- components/SmsHelper.php
+++ components/SmsHelper.php
@@ -1,12 +1,6 @@
 <?php
+namespace app\components;
 
-
-namespace common\helpers;
-
-
-use backend\models\SmsApi;
-use backend\models\SmsTemplateList;
-use backend\models\SmsTypeList;
 use Yii;
 
 class SmsHelper
@@ -53,101 +47,4 @@ class SmsHelper
         $otp_response_array = json_decode($otp_response, true);
         $otp_response_success_code = $otp_response_array['api_response_code'];*/
     }
-
-    public static function get_sms_type_status($status)
-    {
-        if ($status) {
-            return "Active";
-        } else {
-            return "Inactive";
-        }
-    }
-
-    public static function get_sms_template_status($status)
-    {
-        if ($status) {
-            return "Active";
-        } else {
-            return "Inactive";
-        }
-    }
-
-    public static function get_sms_type_name($type_id)
-    {
-        $sms_type_model = SmsTypeList::findOne(['id' => $type_id]);
-        if (!empty($sms_type_model)) {
-            return $sms_type_model->name;
-        } else {
-            return '';
-        }
-    }
-
-    public static function sms_type($sms_type)
-    {
-        if ($sms_type) {
-            return "Masking";
-        } else {
-            return "Non-masking";
-        }
-    }
-
-    public static function sms_api_status($status)
-    {
-        if ($status) {
-            return "Active";
-        } else {
-            return "Inactive";
-        }
-    }
-
-    public static function technician_message()
-    {
-        $sms_template_list = SmsTemplateList::find()->where(['sms_type_id'=>1])->one();
-        if(!empty($sms_template_list)){
-            return $sms_template_list->template_content;
-        }else{
-            return '';
-        }
-    }
-
-    public static function client_message()
-    {
-        $sms_template_list = SmsTemplateList::find()->where(['sms_type_id'=>2])->one();
-        if(!empty($sms_template_list)){
-            return $sms_template_list->template_content;
-        }else{
-            return '';
-        }
-    }
-
-    public static function complain_completed_message()
-    {
-        $sms_template_list = SmsTemplateList::find()->where(['sms_type_id'=>3])->one();
-        if(!empty($sms_template_list)){
-            return $sms_template_list->template_content;
-        }else{
-            return '';
-        }
-    }
-
-    public static function registration_sms()
-    {
-        $sms_template_list = SmsTemplateList::find()->where(['sms_type_id'=>4])->one();
-        if(!empty($sms_template_list)){
-            return $sms_template_list->template_content;
-        }else{
-            return '';
-        }
-    }
-
-    public static function bill_sms()
-    {
-        $sms_template_list = SmsTemplateList::find()->where(['sms_type_id'=>5])->one();
-        if(!empty($sms_template_list)){
-            return $sms_template_list->template_content;
-        }else{
-            return '';
-        }
-    }
-
 }
