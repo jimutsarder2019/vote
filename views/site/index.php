7b81f3c0609045fd2746e8fae01802a8c3b394c0
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-18 19:26:22 +0600

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,9 @@
+<?php
+use yii\helpers\Url;
+$baseUrl = Url::base();
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ISPAB Voter List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?=$baseUrl?>/custom/css/style.css">
   </head>
   <body>
 
