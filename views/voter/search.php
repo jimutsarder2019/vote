89ba4af20d7ecb3a341fa6c01efa240a3d0f1d49
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-19 13:52:33 +0600

    fix

--- views/voter/search.php
+++ views/voter/search.php
@@ -43,47 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				<div class="card">
 					<div class="card-body">
 						<div class="user-status table-responsive latest-order-table">
-							<?= GridView::widget([
-								'dataProvider' => $dataProvider,
-								'filterModel' => $searchModel,
-								'columns' => [
-									//['class' => 'yii\grid\SerialColumn'],
 
-									//'id',
-									'company',
-									'name',
-									'ispab_member',
-									'voter_no',
-									'mobile1',
-									//'mobile2',
-									'email:email',
-									//'thana',
-									'district',
-									//'address',
-									//'license',
-									//'date',
-									[
-										'label' => 'Call Done',
-										'headerOptions' => ['style' => 'color:#ff4c3b'],
-										'content' => function ($model) {
-											return ($model->call_done)?'<span style="color:green; font-weight:bold; font-size:16px">Yes</span>':'<a href="?r=voter/call-done&vid='.$model->id.'"><span style="color:red; font-weight:bold; font-size:16px"><i style="color:red" class="fa fa-phone"></i> Call</span></a>';
-										}
-									],
-									[
-										'label' => 'Visit Done',
-										'headerOptions' => ['style' => 'color:#ff4c3b'],
-										'content' => function ($model) {
-											return ($model->visit_done)?'<span style="color:green; font-weight:bold; font-size:16px">Yes</span>':'<a href="?r=voter/visit-done&vid='.$model->id.'"><span style="color:red; font-weight:bold; font-size:16px"><i style="color:red" class="fa fa-eye"></i>Visit</span></a>';
-										}
-									],
-									[
-										'class' => ActionColumn::className(),
-										'urlCreator' => function ($action, Voter $model, $key, $index, $column) {
-											return Url::toRoute([$action, 'id' => $model->id]);
-										 }
-									],
-								],
-							]); ?>
 						</div>
 					</div>
 				</div>
