b0528629f8949ea3f1cbaf491735dcd4ddb8ea3b
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-21 14:20:50 +0600

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -166,7 +166,7 @@ $baseUrl = Url::base();
 </footer>
 
 
-
+<?php if(isset($popup) && $popup){?>
 <div class="onload-popup">
   <div id="myModalOnload" class="modal fade" tabindex="-1">
         <div class="modal-dialog">
@@ -181,6 +181,7 @@ $baseUrl = Url::base();
         </div>
     </div>
 </div>
+<?php } ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
