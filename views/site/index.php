268380c6767f3bf507e86d2ee64c9a47b6d1a6a2
jimutsarder2019 <jimutsarder@gmail.com>
2024-02-21 14:47:25 +0600

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -12,6 +12,9 @@ $baseUrl = Url::base();
     <title>ISPAB Voter List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?=$baseUrl?>/custom/css/style.css">
+	
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  
   </head>
   <body>
 
@@ -185,6 +188,7 @@ $baseUrl = Url::base();
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 </head>
 
     <script>
